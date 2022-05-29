65b343f734daef29f73c6b717a22abf409826661
Martin Leggewie <trommelmeister@gmx.de>
2022-05-29 17:54:53 +0200

    remove Python remains

--- python-for-reference/DataReader.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-# Liest die rohen Daten aus der XLSX-Datei und bietet dem Benutzer den Zugriff
-# auf alle gelesenen Daten über jeweilige DataFrame-Objekte.
-class DataReader:
-    def __init__(self, args):
-        excelFile = pandas.ExcelFile(args.inputfilename)
-        self.dfBusinessEventDefinitions = pandas.read_excel(excelFile, sheet_name='Business Event Definitions')
-        self.dfObjectModelChanges = pandas.read_excel(excelFile, sheet_name='Object Model Changes', header=None)
